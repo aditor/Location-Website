4d9413667af7a69bec3c945b53ea7c9a9dc82589
Aditya Kamath <aditya.24@hotmail.com>
2016-07-30 17:02:51 -0700

    Added API functionality

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -8,6 +8,36 @@ var sendJSONResponse = function(res, status, content) {
   res.json(content);
 };
 
+
+var doAddReview = function(req, res, location) {
+  if (!location) {
+    sendJsonResponse(res, 404, {"message": "locationid not found"});
+    //create a data object, and push into the array of review objects
+    } else {
+        location.reviews.push(
+        {
+        author: req.body.author,
+        rating: req.body.rating,
+        reviewText: req.body.reviewText
+        }
+    );
+    //subdocuments cant be saved, parent doc must be saved after review added
+    location.save(function(err, location) {
+      var thisReview;
+      if (err) {
+        sendJsonResponse(res, 400, err);
+      } else {
+        //successful save operation
+        updateAverageRating(location._id);
+        //returned object is the LAST review added (for confirmation)
+        thisReview = location.reviews[location.reviews.length - 1];
+        sendJsonResponse(res, 201, thisReview);
+      }
+    });
+  }
+};
+
+
 module.exports.reviewsCreate = function (req, res) {
   var locationid = req.params.locationid;
   if (locationid) {
@@ -46,18 +76,15 @@ module.exports.reviewsReadOne = function (req, res) {
           sendJSONResponse(res, 404, err);
           return;
         }
-        
         //check if returned location HAS reviews
         if(location.reviews && location.reviews.length > 0){
           //method .id RETURNS the single matching SUBdocument
           review = location.reviews.id(req.params.reviewid);
-          
             if(!review){
             	sendJSONResponse(res, 404, {"message":"reviewid not found"});
             } 
             else{
-            	response = {location : { name : location.name,
-          		                         id : req.params.locationid},
+            	response = {location : { name : location.name, id : req.params.locationid},
           	              review : review};
           	  sendJSONResponse(res, 200, response);
             }
@@ -66,13 +93,46 @@ module.exports.reviewsReadOne = function (req, res) {
         	sendJSONResponse(res, 404, {"message" : "No reviews found"});
         }	
     }); 
-
   }else { 
     sendJSONResponse(res, 404, {"message":"Not found, locationid and reviewid both required"});
   }
 };
  
+//find correct document given the id in *function(Locationid)*
+var updateAverageRating = function(locationid) {
+  Loc
+  .findById(locationid)
+  .select('rating reviews')
+  .exec(
+    function(err, location) {
+      if (!err) {
+        doSetAverageRating(location);
+      }
+  });
+};
 
+var doSetAverageRating = function(location) {
+  var i, reviewCount, ratingAverage, ratingTotal;
+  if (location.reviews && location.reviews.length > 0) {
+    reviewCount = location.reviews.length;
+    ratingTotal = 0;
+    for (i = 0; i < reviewCount; i++) {
+     ratingTotal = ratingTotal + location.reviews[i].rating;
+     } 
+    ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+    location.rating = ratingAverage;
+    location.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Average rating updated to", ratingAverage);
+      }
+    });
+  }
+};
+
+
+//PUT
 module.exports.reviewsUpdateOne = function (req, res) {
   sendJSONResponse(res, 200, {"status" : "success"});
 };
